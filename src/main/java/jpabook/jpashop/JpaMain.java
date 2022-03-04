2aa574fa193a61b94960edce517d947c0238934e
mssdfg0708 <mssdfg0708@naver.com>
2022-03-05 01:05:32 +0900

    연관관계 매핑 기초

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -14,6 +17,13 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+
+        Order order = new Order();
+        em.persist(order);
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrder(order);
+        em.persist(orderItem);
 
         try {
             tx.commit();
